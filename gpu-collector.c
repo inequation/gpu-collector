ef05ddc245c8938984f2eac7af8f73bfe555b18e
Leszek Godlewski <github@inequation.org>
2013-01-20 22:27:31 +0100

    some work

--- gpu-collector.c
+++ gpu-collector.c
@@ -1,7 +1,14 @@
 #include <sys/types.h>
 #include <dirent.h>
+#include <unistd.h>
 #include <stdbool.h>
 #include <stdio.h>
+
+#if 0
+#define debugf printf
+#else
+#define debugf (void)
+#endif
 
 struct gpu_info
 {
@@ -12,42 +19,59 @@ struct gpu_info
 
 struct gpu_info gpu_info[2] = {{0}, {0}};
 
-int main(int argc, char *argv[])
+void found_gpu(unsigned int vendor,
+	unsigned int device, unsigned int memory_bytes)
+{
+	printf("Detected VGA device: vendor = %04x, device = %04x, memory = %d kB\n",
+		vendor, device, memory_bytes / 1024);
+}
+
+int enumerate_gpus(void (* found_callback)(unsigned int vendor,
+	unsigned int device, unsigned int memory_bytes))
 {
 	int i;
-	struct dirent *pci_device_dir;
+	struct dirent *device_dir;
 	char path_buf[PATH_MAX];
-	DIR *pci_devices = opendir("/sys/bus/pci/devices");
+	DIR *pci_devices;
+
+	pci_devices = opendir("/sys/bus/pci/devices");
 	if (!pci_devices)
 	{
-		printf("Failed to access sysfs\n");
+		debugf("Failed to access sysfs\n");
 		return 1;
 	}
-	while ((pci_device_dir = readdir(pci_devices)) != NULL)
+	while ((device_dir = readdir(pci_devices)) != NULL)
 	{
-		DIR *pci_device;
+		DIR *subdir;
 		struct dirent *entry;
 		unsigned int d_class = 0, d_vendor = 0, d_device = 0, d_memory = 0;
 
-		if (pci_device_dir->d_name[0] == '.')
-			continue;
-
-		snprintf(path_buf, sizeof(path_buf), "/sys/bus/pci_devices/%s",
-			pci_device_dir->d_name);
-		printf("Entering %s\n", path_buf);
-		pci_device = opendir(path_buf);
-
-		if (!pci_device)
+		// skip ".", ".." and dotfiles
+		if (device_dir->d_name[0] == '.')
 		{
-			printf("Can't access %s, skipping\n", path_buf);
+			debugf("Dotfile, skipping\n");
 			continue;
 		}
 
-		while ((entry = readdir(pci_device)) != NULL)
+		snprintf(path_buf, sizeof(path_buf), "/sys/bus/pci/devices/%s",
+			device_dir->d_name);
+
+		debugf("Entering %s\n", path_buf);
+		subdir = opendir(path_buf);
+		if (!subdir)
 		{
+			debugf("Can't access %s, skipping\n", path_buf);
+			continue;
+		}
+
+		while ((entry = readdir(subdir)) != NULL)
+		{
+
+			snprintf(path_buf, sizeof(path_buf), "/sys/bus/pci/devices/%s/%s",
+				device_dir->d_name, entry->d_name);
 			if (!strcmp(entry->d_name, "class"))
 			{
-				FILE *f = fopen(entry->d_name, "r");
+				FILE *f = fopen(path_buf, "r");
 				if (!f)
 					continue;
 				fscanf(f, "0x%X", &d_class);
@@ -58,7 +82,7 @@ int main(int argc, char *argv[])
 			}
 			else if (!strcmp(entry->d_name, "vendor"))
 			{
-				FILE *f = fopen(entry->d_name, "r");
+				FILE *f = fopen(path_buf, "r");
 				if (!f)
 					continue;
 				fscanf(f, "0x%X", &d_vendor);
@@ -67,7 +91,7 @@ int main(int argc, char *argv[])
 			}
 			else if (!strcmp(entry->d_name, "device"))
 			{
-				FILE *f = fopen(entry->d_name, "r");
+				FILE *f = fopen(path_buf, "r");
 				if (!f)
 					continue;
 				fscanf(f, "0x%X", &d_device);
@@ -75,24 +99,17 @@ int main(int argc, char *argv[])
 				continue;
 			}
 		}
-		printf("Read PCI device: vendor = %04x, device = %04x\n",
+		if (!d_class)
+			continue;
+		debugf("Detected VGA device: vendor = %04x, device = %04x\n",
 			d_vendor, d_device);
+		found_callback(d_vendor, d_device, d_memory);
 	}
-	/*for (i = 0; i < sizeof(gpu_info) / sizeof(gpu_info[0]); ++i)
-	{
-		if (gpu_info[i].found)
-		{
-			printf("Found %s GPU:\n"
-				"Vendor: 0x%04X\n",
-				"Device: 0x%04X\n"
-				"Memory: %z bytes\n",
-				i == 0 ? "primary" : "secondary",
-				vendor_id,
-				device_id,
-				memory_bytes);
-		}
-	}
-	if (!gpu_info[0].found)
-		printf("Could not find primary GPU\n");*/
+
 	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	return enumerate_gpus(found_gpu);
 }
